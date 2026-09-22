974cd80db5e42a02f590bb5a4caf0750ec4aedb0
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleCloudTextToSpeechV1/AdvancedVoiceOptions.swift
+++ Sources/GoogleCloudTextToSpeechV1/AdvancedVoiceOptions.swift
@@ -264,6 +264,12 @@ public struct AdvancedVoiceOptions: Codable, Equatable, GoogleWKT._AnyPackable,
   }
 
   /// Harm categories that will block the content.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum HarmCategory: Codable, Equatable, Sendable {
     /// Default value. This value is unused.
     case unspecified
@@ -278,15 +284,21 @@ public struct AdvancedVoiceOptions: Codable, Equatable, GoogleWKT._AnyPackable,
     case sexuallyExplicit
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
@@ -384,6 +396,12 @@ public struct AdvancedVoiceOptions: Codable, Equatable, GoogleWKT._AnyPackable,
   }
 
   /// Harm block thresholds for the safety settings.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum HarmBlockThreshold: Codable, Equatable, Sendable {
     /// The harm block threshold is unspecified.
     case unspecified
@@ -399,15 +417,21 @@ public struct AdvancedVoiceOptions: Codable, Equatable, GoogleWKT._AnyPackable,
     case off
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
--- Sources/GoogleCloudTextToSpeechV1/SsmlVoiceGender.swift
+++ Sources/GoogleCloudTextToSpeechV1/SsmlVoiceGender.swift
@@ -18,6 +18,12 @@ import Foundation
 
 /// Gender of the voice as described in
 /// [SSML voice element](https://www.w3.org/TR/speech-synthesis11/#edef_voice).
+///
+/// - Note: Adding cases to this enumeration is not considered a breaking change.
+///   Always include an `@unknown default:` case when switching over this type.
+///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+///   expecting specific values to remain unparsed; future releases may promote
+///   them to named cases.
 public enum SsmlVoiceGender: Codable, Equatable, Sendable {
   /// An unspecified gender.
   /// In VoiceSelectionParams, this means that the client doesn't care which
@@ -33,15 +39,21 @@ public enum SsmlVoiceGender: Codable, Equatable, Sendable {
   case neutral
   /// Encodes an unknown integer value.
   ///
-  /// The most common cause for an unknown values is for the service to send
+  /// The most common cause for an unknown value is for the service to send
   /// a value unknown to the library. We recommend you update your library to
   /// the latest version.
+  ///
+  /// - Warning: Do not pattern-match specific integer values in this case;
+  ///   future releases may promote them to named enum cases.
   case unknownIntValue(Int)
   /// Encodes an unknown string value.
   ///
-  /// The most common cause for an unknown values is for the service to send
+  /// The most common cause for an unknown value is for the service to send
   /// a value unknown to the library. We recommend you update your library to
   /// the latest version.
+  ///
+  /// - Warning: Do not pattern-match specific string literals in this case;
+  ///   future releases may promote them to named enum cases.
   case unknownStringValue(String)
 
   public init() {
